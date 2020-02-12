55b84dffe052c6c5335e51d27be00dfa49d3f334
Tom Cizeron <tom.cizeron@edhec.com>
2020-02-12 17:22:34 +0100

    Yummy application

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,15 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
 end
+
+# namespace :admin do
+#   resources :restaurants do
+#     resources :reviews
+#   end
+# end
+
+# namespace :user do
+#   resources :restaurants, only: [:index, :new, :create, :show] do
+#     resources :reviews, only: [:new, :create]
+#   end
+# end
