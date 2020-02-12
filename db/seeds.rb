4f0b345de3d4228af2d556bc223762ff771cbad3
Tom Cizeron <tom.cizeron@edhec.com>
2020-02-12 12:33:00 +0100

    model ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0635374849',
+    category:       'italian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '0600524829',
+    category:        'chinese'
+  },
+  {
+    name:         'Big Fernand',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0612176658',
+    category:       'french'
+  },
+  {
+    name:         'Subway',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0644267781',
+    category:       'belgian'
+  },
+  {
+    name:         'Mc Donalds',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0632009866',
+    category:       'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
